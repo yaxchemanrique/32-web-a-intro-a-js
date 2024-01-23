9a80523a75c44d1fbd624ff7a58d192a10126ee3
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-01-22 19:31:52 -0800

    feat: tipos de datos

---
+++ 1.2-tipos-de-datos/app.js
@@ -0,0 +1,71 @@
+/* 
+Por el moento, no vamos a usar VAR
+var firstName = 'Yaxche';
+*/
+
+// Decalaración de una variable
+// Mala práctica
+let firstName;
+// Asignación de una variable
+firstName = 'Yaxche';
+
+// Declaración y asignacion de una variable
+let lastName = 'Manrique';
+console.log(lastName);
+
+
+const insta = '@yaxchemanrique';
+
+lastName = 'Rios';
+
+// console.log(firstame);
+// console.log('linea 22');
+// console.log(lastName);
+// console.log(insta);
+
+let string = 'Este es un string de texto';
+let number = 5;
+let boolean = true;
+
+// console.log(string);
+// console.log(typeof string);
+
+// console.log(number);
+// console.log(typeof number);
+
+// console.log(boolean);
+// console.log(typeof boolean);
+
+let suma = 2 + 5;
+// console.log(suma);
+
+let resta = 8 - 10;
+// console.log(resta);
+
+let multiplicacion = 7 * 4;
+// console.log(multiplicacion);
+
+let division = 5 / 8;
+// console.log(division);
+
+let modulo = 8 % 5;
+// console.log(modulo);
+
+let moduloDos = 14 % 5;
+// console.log(moduloDos);
+
+let exponencial = 2 ** 8;
+// console.log(exponencial);
+
+const a = false;
+const b = 8;
+console.log(a / b);
+
+let agePrompt = prompt('Cual es tu edad?');
+// let agePrompt = Number(prompt('Cual es tu edad?'));
+let age = Number(agePrompt);
+// Cuantos años tendra dentro de 10 años?
+
+let tiempoFuturo = 10;
+console.log('En 10 años, tendrás');
+console.log(age + tiempoFuturo);
