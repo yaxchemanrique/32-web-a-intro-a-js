02d5e46f111f5ed0d855b1bf5ab731b1bfb25305
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-02-12 19:28:09 -0800

    feat: funcionalidad de comparacion de score

--- bootstrap/app.js
+++ bootstrap/app.js
@@ -5,29 +5,40 @@ const input = document.querySelector('#floatingInput');
 const checkButton = document.querySelector('#check-button');
 // const hintContainer = document.querySelector('#hint');
 const hintContainer = document.querySelector('[role="alert"]');
+const scoreContainer = document.querySelector('#score');
+let score = 5;
 
-// randomNumberContainer.innerText = randomNumber;
+scoreContainer.innerText = score;
+randomNumberContainer.innerText = randomNumber;
 function checkResult() {
-    // console.log('Ejecutado!');
+
     const response = Number(input.value);
 
     if (response > 10 || response < 0) {
         hintContainer.innerHTML = '☝️ Recuerda que todos los numeros están entre el 0 y el 10';
         hintContainer.classList = 'alert alert-danger';
+        score--;
+        scoreContainer.innerText = score;
         return;
     }
 
     if (randomNumber === response) {
         hintContainer.innerHTML = '✅ Excelente! Le atinaste';
         hintContainer.classList = 'alert alert-success';
+        randomNumberContainer.innerText = randomNumber;
 
     } else if (randomNumber > response) {
         hintContainer.innerHTML = '📈 Intenta un numéro más grande';
         hintContainer.classList = 'alert alert-warning';
+        score--;
+        scoreContainer.innerText = score;
 
     } else if (randomNumber < response) {
         hintContainer.innerHTML = '📉 Intenta un numéro más pequeño';
         hintContainer.classList = 'alert alert-warning';
+        score--;
+        scoreContainer.innerText = score;
+
     }
 }
 
